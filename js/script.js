c0f92e424bdcd4fe1981dba8ba33762efccd19e6
TienPhamDev <manhtienbn18891@gmail.com>
2024-02-29 22:53:31 -0500

    js

--- js/script.js
+++ js/script.js
@@ -36,17 +36,29 @@ multiStep.addEventListener("click", (e) => {
       nextStep(multiStepChild, "step1", "step2", "66.66%");
     }
   } else if (e.target.className === "btn-next-2") {
-    if (inputName.value === "") {
+    if (inputEmail.value === "") {
       warningEmail.classList.remove("hidden");
     }else {
       warningEmail.classList.add("hidden");
       nextStep(multiStepChild, "step2", "step3", "100%");
     }
     
-  } else if (e.target.className === "btn-previous-2") {
-    prevStep(multiStepChild, "step2", "step1", "33.33%");
-  } else if (e.target.className === "btn-previous-3") {
-    prevStep(multiStepChild, "step3", "step2", "66.66%");
+  } else if (e.target.className=== "btn-submit"){
+    if (inputPhone.value === "") {
+      warningPhone.classList.remove("hidden");
+    }
   }
+    else if (e.target.className === "btn-previous-2") {
+    if (inputEmail.value !== "") {
+      warningEmail.classList.add("hidden");
+    }  
+    prevStep(multiStepChild, "step2", "step1", "33.33%");
+    
+  } else if (e.target.className === "btn-previous-3") {
+    if (inputEmail.value !== "") {
+      warningPhone.classList.add("hidden");
+    } 
+    prevStep(multiStepChild, "step3", "step2", "66.66%");
+  } 
   
 });
